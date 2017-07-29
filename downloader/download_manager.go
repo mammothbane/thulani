b892dd48234dcbed539ddb982af4a76e81d2e8a1
Nathan Perry <avaglir@gmail.com>
2017-07-29 17:12:30 -0400

    simplify further

--- downloader/download_manager.go
+++ downloader/download_manager.go
@@ -25,14 +25,8 @@ type DownloadManager struct {
 	voiceID string
 	dls     chan *downloader
 
-	PlayState      chan DlMessage
-	playStateChan  chan DlMessage
-	proxyStateChan chan DlMessage
-
-	proxyChan chan playBundle
+	PlayState chan DlMessage
 }
-
-const proxyBufSize = 512
 
 func NewManager(s *discordgo.Session, guildID string, voiceChanID string) *DownloadManager {
 	dm := &DownloadManager{
@@ -41,77 +35,58 @@ func NewManager(s *discordgo.Session, guildID string, voiceChanID string) *Downl
 		PlayState: make(chan DlMessage),
 		guildID:   guildID,
 		voiceID:   voiceChanID,
-
-		playStateChan: make(chan DlMessage),
-
-		proxyStateChan: make(chan DlMessage),
-		proxyChan:      make(chan playBundle),
 	}
 
-	go dm.teeStateMessages()
-	go dm.proxyOpusPackets()
 	go dm.playFromQueue()
 
 	return dm
 }
 
-func (m *DownloadManager) teeStateMessages() {
-	for msg := range m.PlayState {
-		m.playStateChan <- msg
-		m.proxyStateChan <- msg
-	}
-}
-
-func (m *DownloadManager) proxyOpusPackets() {
-loop:
-	for bundle := range m.proxyChan {
-		playState := Play
-		bundle.conn.Speaking(true)
-
-		cleanup := func() {
-			bundle.conn.Speaking(false)
-			bundle.conn.Disconnect()
-		}
-
-		for {
-			switch playState {
-			case Clear:
-				for {
-					select {
-					case <-m.proxyChan:
-					default:
-					}
-				}
-				cleanup()
-				continue loop
-			case Pause:
-				playState = <-m.proxyStateChan
-			case Play:
-				select { // first check if we have a state update message coming in
-				case playState = <-m.proxyStateChan:
-				case bundle.conn.OpusSend <- <-bundle.data:
-				}
-			}
-		}
-		cleanup()
-	}
-}
-
 func (m *DownloadManager) playFromQueue() {
 	for dl := range m.dls {
-		ch, err := m.session.ChannelVoiceJoin(m.guildID, m.voiceID, false, false)
+		conn, err := m.session.ChannelVoiceJoin(m.guildID, m.voiceID, false, false)
 		if err != nil {
 			log.Errorf("unable to connect to the voice channel: %q", err)
 			time.Sleep(1 * time.Second)
 			break
 		}
 
-		out, done := dl.Start()
-		m.proxyChan <- playBundle{
-			data: out,
-			conn: ch,
+		out, _ := dl.Start()
+
+		playState := Play
+		conn.Speaking(true)
+
+		cleanup := func() {
+			conn.Speaking(false)
+			conn.Disconnect()
 		}
-		<-done
+
+	inner:
+		for {
+			switch playState {
+			case Clear:
+				for {
+					select {
+					case <-m.PlayState:
+					default:
+					}
+				}
+				break inner
+			case Pause:
+				playState = <-m.PlayState
+			case Play:
+				select { // first check if we have a state update message coming in
+				case playState = <-m.PlayState:
+				case elem, ok := <-out:
+					if !ok {
+						break inner
+					}
+
+					conn.OpusSend <- elem
+				}
+			}
+		}
+		cleanup()
 	}
 }
 
