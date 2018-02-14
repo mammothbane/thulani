66eeaaab1777c6bb85627c2a41fd5234a7576f84
Nathan Perry <avaglir@gmail.com>
2018-02-14 16:20:54 -0500

    remove color logging from file

--- src/main.rs
+++ src/main.rs
@@ -118,22 +118,32 @@ fn main() {
         .trace(Color::BrightMagenta);
 
     fern::Dispatch::new()
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "{} [{}] [{}] {}",
-                chrono::Local::now().format("%_m/%_d/%y %l:%M:%S%P"),
-                colors.color(record.level()),
-                record.target(),
-                message
-            ))
-        })
         .level_for("serenity::voice::connection", log::LevelFilter::Error)
         .chain(fern::Dispatch::new()
+            .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "{} [{}] [{}] {}",
+                    chrono::Local::now().format("%_m/%_d/%y %l:%M:%S%P"),
+                    colors.color(record.level()),
+                    record.target(),
+                    message
+                ))
+            })
             .level(log::LevelFilter::Warn)
             .level_for("thulani", log::LevelFilter::Debug)
             .chain(std::io::stdout())
         )
         .chain(fern::Dispatch::new()
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "{} [{}] [{}] {}",
+                    chrono::Local::now().format("%_m/%_d/%y %l:%M:%S%P"),
+                    record.level(),
+                    record.target(),
+                    message
+                ))
+            })
+
             .level(log::LevelFilter::Info)
             .level_for("thulani", log::LevelFilter::Trace)
             .chain(fern::log_file("thulani.log").expect("problem creating log file"))
