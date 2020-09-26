1b01a8a8c86de0e5d958ae5a62c9b7e98ac7d5e3
Nathan Perry <np@nathanperry.dev>
2020-09-26 16:18:52 -0400

    extend/fixup pianoman

--- src/commands/today/pianoman.rs
+++ src/commands/today/pianoman.rs
@@ -2,10 +2,19 @@ use super::prelude::*;
 
 lazy_static! {
     static ref TARGET_TIME: chrono::NaiveTime = chrono::NaiveTime::from_hms(21, 0, 0);
+
+    static ref PIANOMANS: Vec<TodayArgs> = vec![
+        by_url("https://www.youtube.com/watch?v=gxEPV4kolz0"),
+        TodayArgs {
+            url: "https://www.youtube.com/watch?v=gxEPV4kolz0",
+            start: Some(Duration::seconds(30)),
+            end: Some(Duration::seconds(34)),
+        }
+    ];
 }
 
 pub fn pianoman(dt: chrono::NaiveDateTime) -> TodayIter {
-    if dt.weekday() != chrono::Weekday::Fri {
+    if dt.weekday() != chrono::Weekday::Sat {
         return Box::new(empty());
     }
 
@@ -22,5 +31,5 @@ pub fn pianoman(dt: chrono::NaiveDateTime) -> TodayIter {
         return Box::new(empty());
     }
 
-    Box::new(once(by_url("https://www.youtube.com/watch?v=gxEPV4kolz0")))
+    Box::new(PIANOMANS.iter().cloned())
 }
